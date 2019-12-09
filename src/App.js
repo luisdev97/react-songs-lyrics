2bc01954ab6bc113ac09255fb8a14024fb9d0240
luisdev97 <amadormontorodavid@gmail.com>
2019-12-09 15:20:45 +0100

    Get lyrics from API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Form from './components/Form';
 
 const App = () => {
 
   const [ state, setState ] = useState({});
 
-  const getLyric = dataSearch => {
-    console.log(dataSearch);
+  const getLyric = async(dataSearch) => {
+    const { artist, song } = dataSearch;
+    const url = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+    const res = await axios.get(url);
+
+    setState({
+      ...state,
+      lyric: res.data.lyrics
+    })
   }
 
   return (
