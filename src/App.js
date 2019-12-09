19df113fd84ef5b958ddc8f50b878c0785f752bb
luisdev97 <amadormontorodavid@gmail.com>
2019-12-09 14:56:00 +0100

    Form component created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import Form from './components/Form';
+
+const App = () => {
+  return (
+    <>
+      <Form/>
+    </>
+  );
+};
+
+export default App;
