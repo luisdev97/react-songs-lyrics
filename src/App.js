3f40a4730b49f850bd5beb7279fca08c4fbf2655
luisdev97 <amadormontorodavid@gmail.com>
2019-12-09 16:12:34 +0100

    Get artistInfo from API

--- src/App.js
+++ src/App.js
@@ -5,32 +5,44 @@ import Song from './components/Song';
 
 const App = () => {
 
-  const [ state, setState ] = useState({});
+  const [ lyrics, setLyrics ] = useState('');
+  const [ artist, setArtist ] = useState('');
+  const [ artistInfo, setArtistInfo ] = useState({}); 
 
   const getLyric = async(dataSearch) => {
     const { artist, song } = dataSearch;
-    const url = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+    const urlLyrics = `https://api.lyrics.ovh/v1/${artist}/${song}`;
 
-    const res = await axios.get(url);
+    const songInfo = await axios.get(urlLyrics);
+    setLyrics(songInfo.data.lyrics)
+    setArtist(artist);
 
-    setState({
-      ...state,
-      lyrics: res.data.lyrics
-    })
   }
+
+  const getArtistsInfo = async(artist) => {
+    const url = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
+    const res = await axios.get(url);
+    const artistInfo = res.data.artists[0];
+    setArtistInfo(artistInfo);
+  }
+
+  useEffect( () => {
+    getArtistsInfo(artist);
+  },[artist])
+
 
   return (
     <>
       <Form getLyric={ getLyric }/>
       <div className="container mt-5">
-        
+
         <div className="row">
 
           <div className="col-md-6">
 
           </div>
 
-            <Song lyrics={ state.lyrics }/>
+            <Song lyrics={ lyrics }/>
 
         </div>
       </div>
